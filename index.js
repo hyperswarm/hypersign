8e9c2e1246cb382cb640a8f9fba553c63c27bc13
David Mark Clements <huperekchuno@googlemail.com>
2019-08-14 01:15:01 +0200

    better naming

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const {
   randombytes_buf: randomBytes
 } = require('sodium-universal')
 
-// PUT_VALUE_MAX_SIZE + packet overhead (i.e. the key etc.)
+// VALUE_MAX_SIZE + packet overhead (i.e. the key etc.)
 // should be less than the network MTU, normally 1400 bytes
-const MAX_VALUE_SIZE = 1000
+const VALUE_MAX_SIZE = 1000
 
 class Hypersign {
   salt (str = null, size = 32) {
@@ -40,7 +40,7 @@ class Hypersign {
   sign (value, opts) {
     assert(typeof opts === 'object', 'Options are required')
     assert(Buffer.isBuffer(value), 'Value must be a buffer')
-    assert(value.length <= MAX_VALUE_SIZE, `Value size must be <= ${MAX_VALUE_SIZE}`)
+    assert(value.length <= VALUE_MAX_SIZE, `Value size must be <= ${VALUE_MAX_SIZE}`)
     const { keypair } = opts
     assert(keypair, 'keypair is required')
     const { secretKey, publicKey } = keypair
@@ -55,7 +55,7 @@ class Hypersign {
   signable (value, opts = {}) {
     const { salt } = opts
     assert(Buffer.isBuffer(value), 'Value must be a buffer')
-    assert(value.length <= MAX_VALUE_SIZE, `Value size must be <= ${MAX_VALUE_SIZE}`)
+    assert(value.length <= VALUE_MAX_SIZE, `Value size must be <= ${VALUE_MAX_SIZE}`)
     if (!salt) return value
     assert(Buffer.isBuffer(salt), 'salt must be a buffer')
     assert(
@@ -68,4 +68,4 @@ class Hypersign {
 
 module.exports = () => new Hypersign()
 module.exports.Hypersign = Hypersign
-module.exports.MAX_VALUE_SIZE = MAX_VALUE_SIZE
+module.exports.VALUE_MAX_SIZE = VALUE_MAX_SIZE
